4a75277df4ffd334cc6622794a73d6f2702752e3
TZ MP3 MEDIA <48158172+tzmp3media@users.noreply.github.com>
2019-04-18 18:02:35 +0300

    Add files via upload

---
+++ featured.js
@@ -0,0 +1,17 @@
+/*Javascript for my featured posts...*/
+var total_images = 10;
+var random_number = Math.floor((Math.random()*total_images));
+var random_img = new Array();
+random_img[0] = '<a href="https://www.tzmp3.co.tz/search?q=Diamond Platnumz"><img src="https://cloudup.com/files/immynnzj5Te/download"><h2>Download all Diamond Platnumz New songs 2019</h2></a>';
+random_img[1] = '<a href="https://www.tzmp3.co.tz/search?q=Lady Jaydee"><img src="https://cloudup.com/files/iR73sgx39pE/download"><h2>Download all Lady Jaydee New songs 2019</h2></a>';
+random_img[2] = '<a href="https://www.tzmp3.co.tz/search?q=Harmonize"><img src="https://cloudup.com/files/iQTgZD-veRC/download"><h2>Download all Harmonize New songs Collection 2019</h2></a>';
+random_img[3] = '<a href="https://www.tzmp3.co.tz/search?q=Aslay"><img src="https://cloudup.com/files/i7VKXBbIanX/download"><h2>Download Aslay all New songs 2019</h2></a>';
+random_img[4] = '<a href="https://www.tzmp3.co.tz/search?q=Nandy"><img src="https://cloudup.com/files/iXVuBXvrYEn/download"><h2>Nandy all New songs Downloads 2019</h2></a>';
+random_img[5] = '<a href="https://www.tzmp3.co.tz/search?q=Beka Flavour"><img src="https://cloudup.com/files/i_jaW8fN99T/download"><h2>Download all Beka Flavour New songs 2019</h2></a>';
+random_img[6] = '<a href="https://www.tzmp3.co.tz/search?q=Rayvanny"><img src="https://cloudup.com/files/iGQA6fjy0Gy/download"><h2>Rayvanny all New Songs Downloads 2019</h2></a>';
+random_img[7] = '<a href="https://www.tzmp3.co.tz/search?q=Ruby"><img src="https://cloudup.com/files/iUh6_CHvjLR/download"><h2>Ruby all New songs Collection 2019</h2></a>';
+random_img[8] = '<a href="https://www.tzmp3.co.tz/search?q=Alikiba"><img src="https://cloudup.com/files/iAB4ip6pSEa/download"><h2>Alikiba new Songs Downloads 2019</h2></a>';
+
+random_img[9] = '<a href="https://www.tzmp3.co.tz/search?q=Mbosso"><img src="https://cloudup.com/files/i8IKdoFUUat/download"><h2>Mbosso all Latest Songs downloads 2019</h2></a>';
+
+document.write(random_img[random_number]);
